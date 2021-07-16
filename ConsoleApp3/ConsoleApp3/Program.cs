6cd9249790f9bfbb4091ee12fc630e8628e76d04
Ine <ines.fmallada@gmail.com>
2021-07-16 20:19:02 +0200

    Subir ficheros

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    //Empezar programa
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
