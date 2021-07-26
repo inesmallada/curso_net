f42b9057529151b3a2a0369da61e9e2c779cf043
Ine <ines.fmallada@gmail.com>
2021-07-26 17:12:35 +0200

    Botones de la aplicación

--- InyeccionSQLWPF/InyeccionSQL/InyeccionSQL/MainWindow.xaml.cs
+++ InyeccionSQLWPF/InyeccionSQL/InyeccionSQL/MainWindow.xaml.cs
@@ -26,24 +26,99 @@ namespace InyeccionSQL
         {
             InitializeComponent();
         }
+        private void BtnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            conexion.Open();
+            string ID = TxtIDEliminar.Text;
+            string cadena = "select id, nombre, edad from Personas where id=" + ID;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            SqlDataReader registro = comando.ExecuteReader();
+            if (registro.Read())
+            {
+                LNombre.Content = registro["nombre"].ToString();
+                LEdad.Content = registro["edad"].ToString();
+                BtnBorrar.IsEnabled = true;
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+            }
+            else
+                MessageBox.Show("No existe un artículo con el código ingresado");
+            conexion.Close();
+        }
+
+        private void BtnInsertar_Click(object sender, RoutedEventArgs e)
         {
             conexion.Open();
             string ID = TxtIDInsertar.Text;
             string nombre = TxtNombreInsertar.Text;
             string edad = TxtEdadInsertar.Text;
-            string cadena = "insert into Personas(id, nombre, edad)" + "values("+ID+",'" + nombre+ "'," + edad+ ")";
+            string cadena = "insert into Personas(id, nombre, edad)" + "values(" + ID + ",'" + nombre + "'," + edad + ")";
 
             SqlCommand comando = new SqlCommand(cadena, conexion);
             comando.ExecuteNonQuery();
             MessageBox.Show("Los datos se guardaron correctamente");
             TxtIDInsertar.Text = "";
-            TxtNombreInsertar.Text="";
+            TxtNombreInsertar.Text = "";
             TxtEdadInsertar.Text = "";
             conexion.Close();
+        }
 
+        private void BtnModificar_Click(object sender, RoutedEventArgs e)
+        {
+            conexion.Open();
+            string ID = TxtIDInsertar.Text;
+            string nombre = TxtNombreInsertar.Text;
+            string edad = TxtEdadInsertar.Text;
+            string cadena = "Update Personas set nombre='" + nombre + "', edad=" + edad + "where id=" + ID;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            int cant;
+            cant = comando.ExecuteNonQuery();
+            if (cant == 1)
+            {
+                MessageBox.Show("Se modificaron los datos del artículo");
+                TxtIDInsertar.Text = "";
+                TxtNombreInsertar.Text = "";
+                TxtEdadInsertar.Text = "";
+            }
+            else
+                MessageBox.Show("No existe un artículo con el código ingresado");
+            conexion.Close();
+            //desconozco cuál es el botón para IsEnabled=false
+        }
 
+        private void BtnBorrar_Click(object sender, RoutedEventArgs e)
+        {
+            conexion.Open();
+            string ID= TxtIDInsertar.Text;
+            string cadena = "delete from Personas where id=" + ID;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            int cant;
+            cant = comando.ExecuteNonQuery();
+            if (cant == 1)
+            {
+                LNombre.Content = "";
+                LEdad.Content = "";
+                MessageBox.Show("Se borró el artículo");
+            }
+            else
+                MessageBox.Show("no existe un artículo con el código ingresado");
+            conexion.Close();
+        }
+
+        private void BtnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            conexion.Open();
+            string cod = TxtIDModificar.Text;
+            string cadena = "select id, nombre, edad from Personas where id=" + cod;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            SqlDataReader registro = comando.ExecuteReader();
+            if (registro.Read())
+            {
+                TxtNombreInsertar.Text = registro["nombre"].ToString();
+                TxtEdadInsertar.Text = registro["edad"].ToString();
+                //no sé qué botón IsEnabled=true
+            }
+            else
+                MessageBox.Show("No existe un artículo con el código ingresado");
         }
     }
 }
