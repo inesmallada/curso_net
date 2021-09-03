a4899119c447f38b3007075aa705a71914545b2e
Ine <ines.fmallada@gmail.com>
2021-09-03 18:54:33 +0200

    ejemplo Razor y MVC

---
+++ EFMigrations/EFMigrations/Migrations/202109031407011_Viernes.cs
@@ -0,0 +1,16 @@
+namespace EFMigrations.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Viernes : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ MVC_RAZOR/MVC_RAZOR/Controllers/DemoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_RAZOR.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            ViewBag.Mensaje = "Acceso al método por defecto";
+            return View();
+        }
+    }
+}
